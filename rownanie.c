0f62caa5ef4b036b7197b52eecc911b1d2076713
JacekK87 <jacekielar1@wp.pl>
2017-11-23 08:57:23 +0100

    wyznacznik

---
+++ rownanie.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int wyznacznik (int a1, int b2, int a2, int b1);{
+	printf("Pierwszy wyznacznik: %i\n" , a1*b2 -b1*a2);
+	return a1*b2 -a2*b1;
+} 
+int wyznacznik_x (int c1, int b2, int c2, int b1);{
+        printf("Pierwszy wyznacznik: %i\n" , c1*b2 - b1*c2); 
+        return a1*b2 -a2*b1;	int a1,a2, b1, b2, c1, c2;
+	int w, wx ,wy;
+	{ 
+		puts("Program do oblicznia równań za pomoca wyznaczników");
+
+		printf( "Podaj wartoscc liczby a1 = %f"); scanf( "%f", $a1);
+		printf( "Podaj wartoscc liczby a2 = %f"); scanf( "%f", $a2);
+		printf( "Podaj wartoscc liczby b1 = %f"); scanf( "%f", $b1); 
+		printf( "Podaj wartoscc liczby b2 = %f"); scanf( "%f", $b2);
+		printf( "Podaj wartoscc liczby c1 = %f"); scanf( "%f", $c1);
+		printf( "Podaj wartoscc liczby c2 = %f"); scanf( "%f", $c2);
+
+		
+
+
+
+
+	}
+
+
+		
